ddf63c9748a8264894563a52ff967087de2255e6
russell88brown <russell88brown@gmail.com>
2017-09-14 19:50:29 +0800

    updated 003

--- 003-LargestPrimeFactor.js
+++ 003-LargestPrimeFactor.js
@@ -5,3 +5,14 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 
+
+function main() {
+	number = 0;
+	primeFactorsArray = [600851475143];
+	priorNumber = 0;
+	number = 600851475143;
+	do {
+	}
+	while
+
+main();
