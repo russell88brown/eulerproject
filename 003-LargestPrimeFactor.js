0382229943ca090dc7795149fc2027f15364b2c6
russell88brown <russell88brown@gmail.com>
2017-09-14 21:43:44 +0800

    cleaned up 003

--- 003-LargestPrimeFactor.js
+++ 003-LargestPrimeFactor.js
@@ -2,46 +2,35 @@
 https://projecteuler.net/problem=3
 The prime factors of 13195 are 5, 7, 13 and 29.
 
-What is the largest prime factor of the number 600851475143 ?
+What is the largest prime factor of the number 600851475143?
 */
 
 
-function main() {
-	number = 600851475143
-	primeFactorArray = [];
+var number = 600851475143;
+var primeFactorArray = []
 
-	for (i = 0; i < 10; i ++) {
-		primeFactorArray.push(nextPrime(number));
-		console.log(primeFactorArray);
-		console.log(primeFactorArray[i]);
-		console.log(number + "=" + number + "/" + primeFactorArray[i])
-		number = number/primeFactorArray[i];
-		console.log(number)
-		if (checkPrime(number) === true) {primeFactorArray.push(number);return primeFactorArray};
-	};
+function largestPrimeFactor() {
+	i = 0
+	do {primeFactorArray.push(nextPrime(number)); number = number/primeFactorArray[i];i++} while (checkPrime(number) === false);
+	primeFactorArray.push(number);
+	console.log(primeFactorArray);
+	return getMaxOfArray(primeFactorArray);
 };
 
-function nextPrime(num) {
-	for (ii = 2; ii < num; ii++) {
-		if ((num/ii) === Math.round(num/ii)) {
-			console.log(ii);
-			return ii;
-		};
-	};
-};
+function nextPrime(num) {for (ii = 2; ii < num; ii++) {if ((num/ii) === Math.round(num/ii)) {return ii;};};};
 
 
 function checkPrime(num) {
 	for (ii = 2; ii < num; ii++) {
 		if ((num/ii) === Math.round(num/ii)) {
-			console.log(false)
 			return false;
 		};
 	};
-	console.log(true)
 	return true
 };
 
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
 
-
-console.log(main());
+console.log(largestPrimeFactor());
